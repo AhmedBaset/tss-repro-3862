553bb24714d7a5a808ee9b2126fbb6752a8f341a
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2025-03-26 21:30:05 +0200

    repro

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { getHeader } from "@tanstack/react-start/server"
 
 export const Route = createFileRoute('/')({
+  beforeLoad: () => {
+    const header = getHeader("Authorization");
+    console.log(header)
+  },
   component: RouteComponent,
 })
 
